310a64ec00933ed4f122a0b793ae7549a76a2df3
dangyee <dangyee@khu.ac.kr>
2021-08-27 22:15:40 +0900

    1655_가운데를_말해요

---
+++ 단기간 성장/1655_가운데를_말해요.c
@@ -0,0 +1,34 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int compare(const void *a, const void *b)    // 오름차순 비교 함수 구현
+{
+    int num1 = *(int *)a;    // void 포인터를 int 포인터로 변환한 뒤 역참조하여 값을 가져옴
+    int num2 = *(int *)b;    // void 포인터를 int 포인터로 변환한 뒤 역참조하여 값을 가져옴
+
+    if (num1 < num2)    // a가 b보다 작을 때는
+        return -1;      // -1 반환
+    
+    if (num1 > num2)    // a가 b보다 클 때는
+        return 1;       // 1 반환
+    
+    return 0;    // a와 b가 같을 때는 0 반환
+}
+
+int main(void)
+{
+    int n, k;
+    int arr[100000] = {0, };
+
+    scanf("%d", &n);
+
+    for(int i = 0; i < n; i++)
+    {  
+        scanf("%d", &k);
+        arr[i] = k;
+        qsort(arr, sizeof(arr) / sizeof(int), sizeof(int), compare);
+        printf("%d\n", (int)arr[i/2]);
+    }
+
+    return 0;
+}
