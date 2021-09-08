d174db0a2d955f727cd81012222d3cf76c3b7542
dangyee <dangyee@khu.ac.kr>
2021-09-08 21:54:10 +0900

    10866_덱

---
+++ Algorithm/10866_덱.cpp
@@ -0,0 +1,49 @@
+#include<iostream>
+
+using namespace std;
+
+int main(void)
+{
+    int n;
+    string str;
+    cin >> n;
+
+    for(int i = 0; i < n; i++)
+    {
+        cin >> str;
+        if(str == "push_front")
+        {
+
+        }
+        else if(str == "push_back")
+        {
+
+        }
+        else if(str == "pop_front")
+        {
+            
+        }
+        else if(str == "pop_back")
+        {
+            
+        }
+        else if(str == "size")
+        {
+            
+        }
+        else if(str == "empty")
+        {
+            
+        }
+        else if(str == "front")
+        {
+            
+        }
+        else if(str == "back")
+        {
+            
+        }
+    } 
+
+    return 0;
+}
