41d31041848b7dfd669d28f23276afc5e354e912
dangyee <dangyee@khu.ac.kr>
2021-09-20 23:45:30 +0900

    11726_2xn_타일링

---
+++ Algorithm/11726_2xn_타일링.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+
+int arr[1001];
+
+int dp(int n)
+{
+    if(n == 1)
+        return 1;
+    if(n == 2)
+        return 2;
+    if(arr[n] != 0) return arr[n];
+    return arr[n] = (dp(n-1) + dp(n-2)) % 10007;
+}
+
+int main(void)
+{
+    int n;
+    scanf("%d", &n);
+    printf("%d", dp(n));
+
+    return 0;
+}
