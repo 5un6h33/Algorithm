4313714b793603590e03586a615d9eff5bfc3c49
dangyee <dangyee@khu.ac.kr>
2021-10-11 23:10:58 +0900

    10845_큐

--- Algorithm/10845_큐.c
+++ Algorithm/10845_큐.c
@@ -1,28 +1,118 @@
-#include<stdio.h>
-#include<string.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
-#define TRUE 1
-#define FALSE 0
+#define ElementType int
 
-//배열 기반 원형 큐
+typedef struct _Queue 
+{
+    int front, rear, size;
+    unsigned capacity;
+    ElementType* array;
+} Queue;
 
+Queue* createQueue(unsigned capacity)
+{
+    Queue* queue = (Queue*)malloc(sizeof(Queue));
+    queue->capacity = capacity;
+    queue->front = queue->size = 0;
+
+    queue->rear = capacity - 1;
+    queue->array = (ElementType*)malloc(queue->capacity * sizeof(ElementType));
+    return queue;
+}
+
+int isFull(Queue* queue)
+{
+    return (queue->size == queue->capacity);
+}
+
+int isEmpty(Queue* queue)
+{
+    return (queue->size == 0);
+}
+
+void enqueue(Queue* queue, int data)
+{
+    if(isFull(queue))
+        return;
+
+    queue->rear = (queue->rear + 1) % queue->capacity;
+    queue->array[queue->rear] = data;
+    queue->size++; 
+}
+
+ElementType dequeue(Queue* queue)
+{
+    if(isEmpty(queue))
+        return -1;
+
+    int data = queue->array[queue->front];
+
+    queue->front = (queue->front + 1) % queue->capacity;
+    queue->size--;
+    
+    return data;
+}
+
+ElementType front(Queue* queue)
+{
+    if(isEmpty(queue)) 
+        return -1;
+    return queue->array[queue->front];
+}
+
+ElementType back(Queue* queue)
+{
+    if(isEmpty(queue)) 
+        return -1;
+    return queue->array[queue->rear];
+}
+
+int size(Queue* queue)
+{
+    return queue->size;
+}
 
 
 int main(void)
 {
-    int i;
+    Queue* queue = createQueue(10000);
+
     int n;
     scanf("%d", &n);
 
     char str[6];
 
-    for(i = 0; i < n; i++)
+    while(n--)
     {
         scanf("%s", str);
 
         if(!strcmp(str, "push"))
         {
-
+            int x;
+            scanf(" %d", &x);
+            enqueue(queue, x);
+        }
+        else if(!strcmp(str, "pop"))
+        {
+            printf("%d\n", dequeue(queue));
+        }
+        else if(!strcmp(str, "size"))
+        {
+            printf("%d\n", size(queue));
+        }
+        else if(!strcmp(str, "empty"))
+        {
+            printf("%d\n", isEmpty(queue));
+        }
+        else if(!strcmp(str, "front"))
+        {
+            printf("%d\n", front(queue));
+        }
+        else if(!strcmp(str, "back"))
+        {
+            printf("%d\n", back(queue));
         }
     }
 
