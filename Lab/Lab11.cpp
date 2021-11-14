130ee74de0b182099b820b6d8dd0c1d20cd7179c
Developer-Yee <79010673+Developer-Yee@users.noreply.github.com>
2021-11-14 23:17:59 +0900

    Add files via upload

--- Lab/Lab11.cpp
+++ Lab/Lab11.cpp
@@ -166,8 +166,9 @@
 //     double x, y;
 
 // public: 
-//     Point(): x(0), y(0){};
-//     Point(int _x, int _y): x(_x), y(_y){};
+//     Point(): x(0), y(0){}
+//     Point(int _x, int _y): x(_x), y(_y){}
+//     ~Point() {}
 //     int getX() const {return this->x;}
 //     int getY() const {return this->y;}
 //     void setPoint(int x, int y)
@@ -175,10 +176,9 @@
 //         this->x = x;
 //         this->y = y;
 //     }
-//     Point square(const Point& pt)
+//     double square()
 //     {
-//         this->x = pt.getX() * pt.getX();
-//         this->y = pt.getY() * pt.getY();
+//         return sqrt(this->x + this->y);
 //     }
 //     Point operator-(const Point& pt)
 //     {
@@ -200,7 +200,9 @@
 
 // int main() {
 // 	int x1 = 0, y1 = 0, x2 = 0, y2 = 0;
-// 	Point* pP1, * pP2, * pP3;
+// 	Point* pP1;
+//     Point* pP2;
+//     Point* pP3;
 
 // 	cout << "첫번째 좌표(x1, y1)를 입력하세요 : ";
 // 	cin >> x1 >> y1;
@@ -213,78 +215,217 @@
 // 	pP3 = new Point(); //x,y가 0으로 초기화
 
 // 	/* 아래의 방식으로도 x, y값 설정이 가능해야 함 */
-// 	//pP1->setPoint(x1, y1);
-// 	//pP1->setPoint(x2, y2);
+// 	// pP1->setPoint(x1, y1);
+//     // pP2->setPoint(x2, y2);
+    
+//     cout << pP1->getX() << " " << pP1->getY() << endl;
+//     cout << pP2->getX() << " " << pP2->getY() << endl;
 // 	/***********************************************/
 
 // 	*pP3 = (*pP1 - *pP2) * (*pP1 - *pP2);
 
 // 	/* pP3을 활용하여 거리값을 구함 */
-// 	cout << "두 좌표 사이의 길이는 " << sqrt(pP3->getX() + pP3->getY()) << "입니다." << endl;
+// 	cout << "두 좌표 사이의 길이는 " << pP3->square() << "입니다." << endl;
 
-//     delete pP1, pP2, pP3;
+//     delete pP1; 
+//     delete pP2;
+//     delete pP3;
 
 // 	return 0;
 // }
 
 
-#include<iostream>
-#include<string>
+// #include<iostream>
+// #include<string>
 
-using namespace std;
+// using namespace std;
 
-class Account {
-    private:
-        string name;
-        string id;
-        int balance;
+// class Account {
+//     private:
+//         string name;
+//         string id;
+//         int balance;
     
-    public:
-        Account(string _name, string _id, int _balance): name(_name), id(_id), balance(_balance) {};
+//     public:
+//         Account(string _name, string _id, int _balance): name(_name), id(_id), balance(_balance) {};
 
-        string getName() const
-        {
-            return this->name;
-        }
-        string getId() const
-        {
-            return this->id;
-        }
-        int getBalance() const
-        {
-            return this->balance;
-        }
-        ostream& operator<<(ostream& cout, Account& account)
-        {
-            return cout << "학번: " << account.getId() << ", 이름: " << account.getName() << ", 잔액: " << account.getBalance() << endl;
-        }
-};
+//         string getName() const
+//         {
+//             return this->name;
+//         }
+//         string getId() const
+//         {
+//             return this->id;
+//         }
+//         int getBalance() const
+//         {
+//             return this->balance;
+//         }
+//         Account operator+(const Account& account)
+//         {
+//             Account result(this->name, this->id, this->balance + account.getBalance());
+//             return result;
+//         }
+//         Account operator-(const Account& account)
+//         {
+//             Account result(this->name, this->id, this->balance - account.getBalance());
+//             return result;
+//         }
+//         Account& operator=(const Account& account)
+//         {
+//             this->name = account.getName();
+//             this->id = account.getId();
+//             this->balance = account.getBalance();
 
-int main() {
-	Account acnt[3] = {
-		Account("장윤호", "2014", 10000),
-		Account("김유민", "2019", 0),
-		Account("박진배", "2013", 5000),
-	};
+//             return *this;
+//         }
+//         friend ostream& operator<<(ostream& cout, Account& account)
+//         {
+//             return cout << "학번: " << account.getId() << ", 이름: " << account.getName() << ", 잔액: " << account.getBalance() << endl;
+//         }
+// };
 
-    string sendId, recvId;
+// int main() {
+// 	Account acnt[3] = {
+// 		Account("장윤호", "2014", 10000),
+// 		Account("김유민", "2019", 0),
+// 		Account("박진배", "2013", 5000),
+// 	};
 
-    while(1)
-    {
-        cout << "돈을 보낼 학생의 학번을 입력하세요: ";
-        cin >> sendId;
-        if(sendId == "종료")
-            break;
-        cout << "돈을 받을 학생의 학번을 입력하세요: ";
-        cin >> recvId;
-        if(sendId == recvId)
-            continue;
-        for()
+//     string sendId, recvId;
+//     int sendIndex, recvIndex;
 
-    }
+//     while(1)
+//     {
+//         sendIndex = recvIndex = -1;
+//         cout << "돈을 보낼 학생의 학번을 입력하세요: ";
+//         cin >> sendId;
+//         if(sendId == "종료")
+//         {
+//             cout << "종료합니다." << endl;
+//             break;
+//         }
+//         cout << "돈을 받을 학생의 학번을 입력하세요: ";
+//         cin >> recvId;
+//         if(sendId == recvId)
+//         {
+//             cout << "학번이 동일합니다." << endl;
+//             continue;
+//         }
+//         for(int i = 0; i < 3; i++)
+//         {
+//             if(acnt[i].getId() == sendId)
+//                 sendIndex = i;
+//             else if(acnt[i].getId() == recvId)
+//                 recvIndex = i;
+//         }
+//         if(sendIndex == -1 || recvIndex == -1)
+//         {
+//             cout << "보내는 학생 혹은 받는 학생의 학번이 존재하지 않습니다.다시 입력해주세요." << endl;
+//             continue;
+//         }
+//         if(acnt[sendIndex].getBalance() <= 0)
+//         {
+//             cout << "보내는 학생의 잔액이 부족합니다." << endl;
+//             continue;
+//         }
+//         acnt[recvIndex] = acnt[recvIndex] + acnt[sendIndex];
+//         acnt[sendIndex] = acnt[sendIndex] - acnt[sendIndex];
 
-    // for(int i = 0; i < 3; i++)
-        // cout << acnt[i];
+//         cout << "보낸 학생의 잔액 => " << acnt[sendIndex];
+//         cout << "받은 학생의 잔액 => " << acnt[recvIndex];
+//     }
 
-    return 0;
-}
+//     for(int i = 0; i < 3; i++)
+//         cout << acnt[i];
+
+//     return 0;
+// }
+
+
+// #include<iostream>
+// #include<string>
+
+// using namespace std;
+
+// class Account {
+//     private: 
+//         string id;
+//         string name;
+//         int balance;
+//     public:
+//         Account(string _id, string _name, int _balance): id(_id), name(_name), balance(_balance) {}
+//         Account(): id(""), name(""), balance(0){}
+//         string getId() const {
+//             return this->id;
+//         }
+//         string getName() const {
+//             return this->name;
+//         }
+//         int getBalance() const {
+//             return this->balance;
+//         }
+//         void setId(string id)
+//         {
+//             this->id = id;
+//         }
+//         void setName(string name)
+//         {
+//             this->name = name;
+//         }
+//         void setBalance(int balance)
+//         {
+//             this->balance = balance;
+//         }
+
+// };
+
+// int main(void)
+// {
+//     int n, balance, sum = 0;
+//     string id, name;
+//     bool flag;
+//     cout << "총 학생 수 입력: ";
+//     cin >> n;
+
+//     Account *account = new Account[n];
+
+//     for(int i = 0; i < n; i++)
+//     {
+//         flag = false;
+//         cout << i + 1 << "번째 학생 계좌 입력 : " << endl;
+//         cout << "학번 : ";
+//         cin >> id;
+//         cout << endl;
+//         cout << "이름 : ";
+//         cin >> name;
+//         cout << endl;
+//         cout << "잔액 : ";
+//         cin >> balance;
+//         cout << "======================" << endl << endl;
+
+//         if(i != 0)
+//         {
+//             for(int j = 0; j < i; j++)
+//             {
+//                 if(account[j].getId() == id || account[j].getName() == name || account[j].getBalance() == balance)
+//                     flag = true;
+//             }
+//         }
+
+//         if(flag)
+//             exit(-1);
+
+//         account[i].setId(id);
+//         account[i].setName(name);
+//         account[i].setBalance(balance);
+//     } 
+
+//     for(int i = 0; i < n; i++)
+//         sum += account[i].getBalance();
+//     cout << "회수된 금액 : " << sum << endl;
+
+//     delete[] account;
+
+//     return 0;
+// }
