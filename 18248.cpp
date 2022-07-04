9f2b7a9592f4b35a04e5e54915597e2077f4a66e
Sunghee Han <79010673+sunghee0204@users.noreply.github.com>
2022-07-04 21:30:14 +0900

    Create 18248.cpp

---
+++ 18248.cpp
@@ -0,0 +1,66 @@
+#include <iostream>
+#include <string>
+#include <queue>
+
+using namespace std;
+
+int n, x;
+string str;
+queue<int> q;
+
+int main(void)
+{
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+
+    // freopen("input.txt", "r", stdin);
+
+    cin >> n;
+    while (n--)
+    {
+        cin >> str;
+        if (str == "push")
+        {
+            cin >> x;
+            q.push(x);
+        }
+        else if (str == "pop")
+        {
+            if (q.empty())
+                cout << -1 << '\n';
+            else
+            {
+                cout << q.front() << '\n';
+                q.pop();
+            }
+        }
+        else if (str == "size")
+        {
+            cout << q.size() << '\n';
+        }
+        else if (str == "empty")
+        {
+            if (q.empty())
+                cout << 1 << '\n';
+            else
+                cout << 0 << '\n';
+        }
+        else if (str == "front")
+        {
+            if (q.empty())
+                cout << -1 << '\n';
+            else
+                cout << q.front() << '\n';
+        }
+        else if (str == "back")
+        {
+            if (q.empty())
+                cout << -1 << '\n';
+            else
+                cout << q.back() << '\n';
+        }
+    }
+
+    return 0;
+}
